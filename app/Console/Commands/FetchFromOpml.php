d3d8d4bd7a8934e04d1d0772f0825fa01fb94736
Daniel Lucia <daniellucia84@gmail.com>
2024-06-28 13:04:40 +0200

    Obtenemos podcasts desde un opml

---
+++ app/Console/Commands/FetchFromOpml.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Feed;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
+
+class FetchFromOpml extends Command
+{
+
+    protected $signature = 'fetch:opml {file}';
+
+    protected $description = 'Obtiene los podcast desde un fichero OPML';
+
+    public function handle()
+    {
+        $file = $this->argument('file');
+
+        if (!Storage::exists($file)) {
+            $this->error('File not exists');
+        }
+
+        $xml = simplexml_load_string(Storage::get($file));
+
+        foreach ($xml->body->outline as $item) {
+            $url = (string)$item['xmlUrl'];
+            Artisan::call('fetch:feed', ['url' => $url]);
+        }
+    }
+}
