b9061c12d64120a9675bcb1225b8c7faffa61ece
Daniel Lucia <daniellucia84@gmail.com>
2024-07-01 07:37:03 +0200

    Agregamos nuevo comando para buscar episodios

---
+++ app/Console/Commands/SearchEpisodeFromFeed.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Feed;
+use Illuminate\Console\Command;
+
+class SearchEpisodeFromFeed extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'search:episodes {url}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Search episodes from feed url';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $url = $this->argument('url');
+        try {
+            $feed = Feed::where('url', $url)->first();
+            $count = $feed->get_new_episodes();
+            $this->info("{$count} found");
+        } catch (\Exception $e) {
+            $this->error('Failed to fetch podcast feed: ' . $e->getMessage());
+        }
+    }
+}
