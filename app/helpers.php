b28d37dd14a4473b3585b171e93a0c14ec58ce5e
Daniel Lúcia <daniellucia84@gmail.com>
2024-07-01 18:12:34 +0200

    Comprobaciones previas

--- app/helpers.php
+++ app/helpers.php
@@ -13,6 +13,38 @@ if (!function_exists('array_from_xml')) {
     function array_from_xml(string $url): array
     {
         $xml = file_get_contents($url);
+        if (!is_valid_xml($xml)) {
+            return [];
+        }
+
         return Xml2Array::convert($xml)->toArray();
+    }
+}
+
+if (!function_exists('is_valid_xml')) {
+    /**
+     * Comprueba si un xml es válido
+     *
+     * @param string $xml
+     * @return boolean
+     */
+    function is_valid_xml(string $xml): bool
+    {
+        libxml_use_internal_errors(true);
+
+        $doc = simplexml_load_string($xml);
+        $xml = explode("\n", $xml);
+
+        if ($doc !== false) {
+            $errors = libxml_get_errors();
+
+            if (!empty($errors)) {
+                return true;
+            }
+
+            libxml_clear_errors();
+        }
+
+        return false;
     }
 }
